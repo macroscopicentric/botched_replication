631a637d02856ffac7508dea705153f16bdc6d75
Rachel King <rachel.b.king@gmail.com>
2016-06-08 16:20:26 -0400

    Add /newest_change endpoint.

--- web_app.py
+++ web_app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, Markup
+from flask import Flask, render_template, Markup,jsonify
+import csv
 
 app = Flask(__name__)
 
@@ -7,6 +8,16 @@ def index():
 	with open('modified_library_of_babel.html', 'r') as f:
 		current_text = f.read()
 	return render_template('index.html', modified_text=Markup(current_text))
+
+@app.route('/newest_change')
+def fetch_newest_change():
+	with open('library_of_babel_changes.csv', 'r') as f:
+		csvreader = csv.reader(f)
+		rows = []
+		for row in csvreader:
+			rows.append(row)
+	newest_change = rows[-1]
+	return jsonify(newest_change)
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0')
